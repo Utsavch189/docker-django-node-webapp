a0814dc5913dda0e1a4f7ad504253d4de420fe1b
Utsav <utsavpokemon9000chatterjee@gmail.com>
2021-11-05 19:13:19 +0530

    third commit

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@hhlw1ap^$gb4)3%7)0a$uu=#2^!-)m#8^&l-2#^bcfiar(!!0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['techu2k21.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','techu2k21.herokuapp.com']
 
 
 # Application definition
