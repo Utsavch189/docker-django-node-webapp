28891b742c02dbf308e4b16e0a88830d43cf803d
Utsav Chatterjee <88131195+Utsavch189@users.noreply.github.com>
2021-11-08 12:32:18 +0530

    Update wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -12,5 +12,5 @@ import os,sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-sys.path.insert(0, os.path.join('app.settings', 'CCMS'))
+
 application = get_wsgi_application()
